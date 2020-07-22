4b0ca11b71413c3d6574b21235187e3c3db50057
Mykola <nazimkoff@gmail.com>
2020-07-22 16:25:50 +0300

    start JS Tab Price

--- src/js/app.js
+++ src/js/app.js
@@ -95,3 +95,39 @@ const   tabsBlog = document.querySelectorAll('.blog__card-wrapper'),
         });
     }
 });
+//TABS PRICE
+const   priceTitle = document.querySelectorAll('.our_price__card-item__title'),
+        pricePrice = document.querySelectorAll('.our_price__card-item__price'),
+        pricePeriod = document.querySelectorAll('.our_price__card-item__period'), 
+        priceText = document.querySelectorAll('.our_price__card-item__text'),
+        pricebutton = document.querySelectorAll('.our_price__card-item__button'),
+        priceCards = document.querySelectorAll('.our_price__card-item'),
+        priceCardparent = document.querySelector('.our_price__card-wrapper');
+
+        function hidePrice () {
+           
+            priceText.forEach (item => {
+                item.style.display = 'none';
+            });
+            pricebutton.forEach (item => {
+                item.style.display = 'none';
+            });
+        }
+        function showPrice (i) {
+            priceText[i].style.display = 'block';
+            pricebutton[i].style.display = 'block';
+
+        }
+        hidePrice ();
+        showPrice (0);
+        priceCardparent.addEventListener('click', () => {
+        const target = event.target;
+        if (target && target.classList.contains('our_price__card-item')) {
+            priceCards.forEach ( (item, i) => {
+                if (target === item) {
+                    hidePrice ();
+                    showPrice(i); 
+                }
+            });
+        }
+    });
